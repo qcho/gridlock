f8667a6d00ee3e5ac6113c8d5a7b7c7695b11778
Juan Pablo Orsay <jporsay@gmail.com>
2017-04-30 22:19:31 -0300

    created helper util to generate binary numbers as arrays

---
+++ preneural/src/gen_bin_dataset.py
@@ -0,0 +1,16 @@
+def gen_bin_array(max_length=2):
+    if max_length < 2:
+        print("Minimum length is 2")
+        exit(1)
+    if max_length > 5:
+        print("Maximum length is 5")
+        exit(1)
+    string_format = "".join(["{:0", str(max_length), "b}"])
+    out = []
+    for x in range(1 << max_length):
+        out.append([int(d) for d in string_format.format(x)])
+    return out
+
+if __name__ == '__main__':
+    dataset = gen_bin_array(3)
+    [print("{},".format(e)) for e in dataset]
