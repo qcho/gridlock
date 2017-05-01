32cd01a4c7f45ec040fb9e9ac05076a073266661
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-01 14:32:47 -0300

    correct file name

--- neural/src/main.py
+++ neural/src/main.py
@@ -1,7 +1,7 @@
 from data_parser import parse
 
 if __name__ == '__main__':
-    data, err = parse("../data/terrain05.dat")
+    data, err = parse("../data/terrain05.data")
     if err is not None:
         print("Error opening file:", err)
         exit(1)
