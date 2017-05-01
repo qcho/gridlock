b27b1f168f826a9372c5c16aa3b415559ef62f76
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-01 14:36:32 -0300

    layer numbers and transference function to network

--- preneural/src/network.py
+++ preneural/src/network.py
@@ -1,6 +1,10 @@
 from typing import List
 
+from transference.transference_function import TransferenceFunction
+
 
 class Network:
-    def __init__(self, layer_configuration: List[int]):
+    def __init__(self, layer_configuration: List[int], transference_fn: TransferenceFunction):
         self.layer_configuration = layer_configuration
+        self.n_layers = len(layer_configuration)
+        self.transference_fn = transference_fn
