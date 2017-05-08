e68c0219c2e1c40f0e480cfbb623d4283daf5b08
alebian <alebezdjian@gmail.com>
2017-05-08 14:06:09 -0300

    Neuron weight from -0.5 to 0.5

--- neural/src/network/network_layer.py
+++ neural/src/network/network_layer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Neuron:
     def __init__(self, n_inputs):
-        self.weights = np.random.rand(n_inputs)
+        self.weights = np.random.rand(n_inputs) - 0.5
         self.bias = 0
         self.output = 0
         self.delta = 0
