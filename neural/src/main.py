0cb1d9dedb98af5c36144bfe0e49e617ff958c49
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-08 14:19:02 -0300

    serializing and deserializing network

--- neural/src/main.py
+++ neural/src/main.py
@@ -1,24 +1,49 @@
+from pickle import Pickler, Unpickler
+
 from data_parser import parse
 from network import Network
 from transference.hyperbolic_tangent import HyperbolicTangent
 from transference.linear_function import LinearFunction
 
-if __name__ == '__main__':
+network_filename = "net.obj"
+should_load_network = False
+
+
+def get_generic_network():
+    return Network(n_inputs=2, layer_configuration=[
+        (7, HyperbolicTangent()),
+        (7, HyperbolicTangent()),
+        (1, LinearFunction())], eta=0.2)
+
+
+def get_parsed_data():
     data, err = parse("../data/terrain05.data")
     if err is not None:
         print("Error opening file:", err)
         exit(1)
-    inputs, results = zip(*[((x[0], x[1]), [x[2]]) for x in data[:20]])
-    network = Network(n_inputs=2, layer_configuration=[
-        (7, HyperbolicTangent()),
-        (7, HyperbolicTangent()),
-        (1, LinearFunction())], eta=0.2)
+    return zip(*[((x[0], x[1]), [x[2]]) for x in data[:20]])
+
+
+def load_network():
+    with open(network_filename, "rb") as fh:
+        return Unpickler(fh).load()
+
+
+def serialize_network():
+    with open(network_filename, "wb") as serialized_network:
+        Pickler(serialized_network, 2).dump(network)
+
+if __name__ == '__main__':
+    inputs, results = get_parsed_data()
+    network = get_generic_network() if not should_load_network else load_network()
+
     network.print_structure()
     print("---------TRAINING---------")
-    network.train(inputs, results, 50000)
+    network.train(inputs, results, 10000)
     print("---------TRAINED---------")
     network.print_structure()
+
     for x_i, result_i in zip(inputs, results):
         print("For {} expecting {} got {}".format(x_i, result_i, network.predict(x_i)))
 
-
+    serialize_network()
