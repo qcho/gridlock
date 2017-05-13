67f20cac4c525bbdb095e896ebade8b6dcd43b18
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-13 11:05:37 -0300

    Delete preneural

--- preneural/src/transference/identity_function.py
+++
@@ -1,9 +0,0 @@
-from transference.transference_function import TransferenceFunction
-
-
-class IdentityFunction(TransferenceFunction):
-    def apply(self, value):
-        return value
-
-    def apply_derived(self, value):
-        TransferenceFunction.apply_derived(self, value)
