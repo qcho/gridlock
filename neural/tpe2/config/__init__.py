6554f37d043b348b8cf4d7f8bcb4d57a4c439e65
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-14 15:12:35 -0300

    add jsonschema validation

--- neural/tpe2/config/__init__.py
+++ neural/tpe2/config/__init__.py
@@ -1,5 +1,6 @@
 import json
 from typing import Optional, Tuple
+from jsonschema import validate
 
 import pkg_resources
 
@@ -7,14 +8,19 @@ from ..network import Network
 from ..data import __data_pkg__
 
 
+def validate_network(json_network):
+    validation_json = json.loads(pkg_resources.resource_string(__data_pkg__, "network.schema.json"))
+    validate(json_network, validation_json)
+
+
 class Config:
     def __init__(self, file_path):
         self._file_path = file_path
 
-    def parse_network(self, path=None) -> Tuple[Optional[Network], Optional[IOError]]:
+    def parse_network(self, path=None) -> Tuple[Optional[Network], Optional[Exception]]:
         try:
             return self._parse(path), None
-        except IOError as err:
+        except Exception as err:
             return None, err
 
     def _parse(self, path):
@@ -22,6 +28,7 @@ class Config:
             json_obj = json.load(stream)
             if 'network' not in json_obj:
                 raise ValueError("Missing network configuration parameters")
+            validate_network(json_obj["network"])
             return Network.create_from_json(json_obj['network'])
 
     def write_network(self, network: Network, file_path=None):
