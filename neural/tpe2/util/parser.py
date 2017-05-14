8a41c0d9c307f34a98788f088c08a3633b857324
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-14 01:19:44 -0300

    extract _parse

--- neural/tpe2/util/parser.py
+++ neural/tpe2/util/parser.py
@@ -9,23 +9,24 @@ def _zip(data):
     return zip(*[((x[0], x[1]), [x[2]]) for x in data])
 
 
+def _parse(file_name) -> Tuple[Optional[List[List[float]]], Optional[IOError]]:
+    """
+    Parse a terrain file, skipping the first line. Expects file to exist
+    :return: array of [x1, x2, z]
+    """
+    try:
+        with open(pkg_resources.resource_filename(__data_pkg__, file_name)) as file_handler:
+            next(file_handler)
+            return [[float(x) for x in line.split(" ") if len(x) > 0] for line in file_handler], None
+    except IOError as err:
+        return None, err
+
+
 class Parser:
     def __init__(self, file_name='terrain05.data'):
         self.file_name = file_name
-        self.data, self.err = self._parse()
+        self.data, self.err = _parse(file_name)
         self.data_size = len(list(self.data))
-
-    def _parse(self) -> Tuple[Optional[List[List[float]]], Optional[IOError]]:
-        """
-        Parse a terrain file, skipping the first line. Expects file to exist
-        :return: array of [x1, x2, z]
-        """
-        try:
-            with open(pkg_resources.resource_filename(__data_pkg__, self.file_name)) as file_handler:
-                next(file_handler)
-                return [[float(x) for x in line.split(" ") if len(x) > 0] for line in file_handler], None
-        except IOError as err:
-            return None, err
 
     def get(self, filter_fn: Callable[List[Any], Any]=None,
             order_fn: Optional[Callable[List[Any], Any]]=None):
