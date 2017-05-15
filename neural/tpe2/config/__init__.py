66ee580a6b7ebb0f969ccca415ac26e64ae71675
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-15 00:03:04 -0300

    update file_path on load

--- neural/tpe2/config/__init__.py
+++ neural/tpe2/config/__init__.py
@@ -16,7 +16,7 @@ def validate_network(json_network):
 
 class Config:
     def __init__(self, file_path):
-        self._file_path = file_path
+        self.file_path = file_path
 
     def parse_network(self, path=None) -> Tuple[Optional[Network], Optional[Exception]]:
         try:
@@ -25,7 +25,8 @@ class Config:
             return None, err
 
     def _parse(self, path):
-        with open(pkg_resources.resource_filename(__data_pkg__, path if path is not None else self._file_path)) as fp:
+        self.file_path = path if path is not None else self.file_path
+        with open(pkg_resources.resource_filename(__data_pkg__, self.file_path), 'r') as fp:
             json_obj = json.load(fp)
             if 'network' not in json_obj:
                 raise ValueError("Missing network configuration parameters")
@@ -33,6 +34,6 @@ class Config:
             return Network.create_from_json(json_obj['network'])
 
     def write_network(self, network: Network, file_path=None):
-        file_path = file_path if file_path is not None else self._file_path
+        file_path = file_path if file_path is not None else self.file_path
         with open(pkg_resources.resource_filename(__data_pkg__, file_path), 'w') as fh:
             json.dump(network.to_json(), fh, indent=2)
