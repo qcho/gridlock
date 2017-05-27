68b0756eb81b149e69d6edff1ea6e820fb11bcbf
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-27 17:08:25 -0300

    Convert Status to Enum

--- genetics/tp3/models/characters/character.py
+++ genetics/tp3/models/characters/character.py
@@ -28,11 +28,11 @@ class Character:
     @staticmethod
     def set_special_modifiers(special_modifiers):
         Character.special_modifiers = {
-            Stats.strength.name: special_modifiers['special_strength'],
-            Stats.agility.name: special_modifiers['special_agility'],
-            Stats.expertise.name: special_modifiers['special_expertise'],
-            Stats.resistance.name: special_modifiers['special_resistance'],
-            Stats.life.name: special_modifiers['special_life'],
+            Stats.STRENGTH: special_modifiers['special_strength'],
+            Stats.AGILITY: special_modifiers['special_agility'],
+            Stats.EXPERTISE: special_modifiers['special_expertise'],
+            Stats.RESISTANCE: special_modifiers['special_resistance'],
+            Stats.LIFE: special_modifiers['special_life'],
         }
 
     def add_item(self, item: Item):
@@ -42,19 +42,24 @@ class Character:
         return np.sum(list(map(function, self.items.values())))
 
     def get_strength(self):
-        return 100 * np.tanh(0.01 * self._items_properties_sum(lambda x: x.strength) * self.special_modifiers[Stats.strength.name])
+        return 100 * np.tanh(
+            0.01 * self._items_properties_sum(lambda x: x.strength) * self.special_modifiers[Stats.STRENGTH])
 
     def get_agility(self):
-        return np.tanh(0.01 * self._items_properties_sum(lambda x: x.agility) * self.special_modifiers[Stats.agility.name])
+        return np.tanh(
+            0.01 * self._items_properties_sum(lambda x: x.agility) * self.special_modifiers[Stats.AGILITY])
 
     def get_expertise(self):
-        return 0.6 * np.tanh(0.01 * self._items_properties_sum(lambda x: x.expertise) * self.special_modifiers[Stats.expertise.name])
+        return 0.6 * np.tanh(
+            0.01 * self._items_properties_sum(lambda x: x.expertise) * self.special_modifiers[Stats.EXPERTISE])
 
     def get_resistance(self):
-        return np.tanh(0.01 * self._items_properties_sum(lambda x: x.resistance) * self.special_modifiers[Stats.resistance.name])
+        return np.tanh(
+            0.01 * self._items_properties_sum(lambda x: x.resistance) * self.special_modifiers[Stats.RESISTANCE])
 
     def get_life(self):
-        return 100 * np.tanh(0.01 * self._items_properties_sum(lambda x: x.life) * self.special_modifiers[Stats.life.name])
+        return 100 * np.tanh(
+            0.01 * self._items_properties_sum(lambda x: x.life) * self.special_modifiers[Stats.LIFE])
 
     def get_attack(self):
         return (self.get_agility() + self.get_expertise()) * self.get_strength() * self.attack_modifier
