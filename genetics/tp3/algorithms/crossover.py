2b1f136c5024df25676ac3698ce12ad20113d69f
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-27 18:02:02 -0300

    Handling correctly height swapping

--- genetics/tp3/algorithms/crossover.py
+++ genetics/tp3/algorithms/crossover.py
@@ -11,9 +11,14 @@ def _swap_item(child_1: Character, child_2: Character, item_type: ItemType):
     child_1.items[item_type], child_2.items[item_type] = child_2.items[item_type], child_1.items[item_type]
 
 
+def _swap_height(child_1: Character, child_2: Character):
+    child_1.height, child_2.height = child_2.height, child_1.height
+
+
 def one_point(char_1: Character, char_2: Character, point: int):
     child_1, child_2 = _spawn_children(char_1, char_2)
-
+    if point == 0:
+        _swap_height(child_1, child_2)
     for i, item_type in enumerate(ItemType):
         if i >= point:
             _swap_item(child_1, child_2, item_type)
@@ -23,7 +28,8 @@ def one_point(char_1: Character, char_2: Character, point: int):
 
 def two_points(char_1: Character, char_2: Character, point_1: int, point_2: int):
     child_1, child_2 = _spawn_children(char_1, char_2)
-
+    if point_1 == 0:
+        _swap_height(child_1, child_2)
     for i, item_type in enumerate(ItemType):
         if i < point_1 or i >= point_2:
             _swap_item(child_1, child_2, item_type)
@@ -33,7 +39,8 @@ def two_points(char_1: Character, char_2: Character, point_1: int, point_2: int)
 
 def uniform(char_1: Character, char_2: Character, probability: int = 0.5):
     child_1, child_2 = _spawn_children(char_1, char_2)
-
+    if random() < probability:
+        _swap_height(child_1, child_2)
     for i, item_type in enumerate(ItemType):
         if random() < probability:
             _swap_item(child_1, child_2, item_type)
