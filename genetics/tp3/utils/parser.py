35d86271fc2c5b037bed0bd1b8fc14e311fef692
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-27 12:27:05 -0300

    remove comment

--- genetics/tp3/utils/parser.py
+++ genetics/tp3/utils/parser.py
@@ -1,10 +1,8 @@
 import pkg_resources
 from data import __data_pkg__
 
+
 def parse(file_name):
-    """
-    Parse a terrain file, skipping the first line. Expects file to exist
-    """
     try:
         with open(pkg_resources.resource_filename(__data_pkg__, file_name)) as file_handler:
             next(file_handler)
