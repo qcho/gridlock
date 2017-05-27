7e350d88a2abc3d200ba325fcd0b0ebf8efd11fe
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-27 17:29:04 -0300

    rename parameters

--- genetics/tp3/algorithms/crossover.py
+++ genetics/tp3/algorithms/crossover.py
@@ -2,94 +2,92 @@ from tp3.models.items import ItemType
 from random import random
 
 
-def _new_characters(character_1, character_2):
-    new_character_1 = character_1.spawn()
-    new_character_2 = character_2.spawn()
-    return new_character_1, new_character_2
+def _new_characters(char_1, char_2):
+    return char_1.spawn(), char_2.spawn()
 
 
-def _change_point(character_1, character_2, new_character_1, new_character_2, i):
+def _change_point(char_1, char_2, child_1, child_2, i):
     # Height, weapons, boots, helmets, gloves, armour
     if i == 0:
-        new_character_2.height = character_1.height
-        new_character_1.height = character_2.height
+        child_2.height = char_1.height
+        child_1.height = char_2.height
     elif i == 1:
-        new_character_2.items[ItemType.WEAPON] = character_1.items[ItemType.WEAPON]
-        new_character_1.items[ItemType.WEAPON] = character_2.items[ItemType.WEAPON]
+        child_2.items[ItemType.WEAPON] = char_1.items[ItemType.WEAPON]
+        child_1.items[ItemType.WEAPON] = char_2.items[ItemType.WEAPON]
     elif i == 2:
-        new_character_2.items[ItemType.BOOTS] = character_1.items[ItemType.BOOTS]
-        new_character_1.items[ItemType.BOOTS] = character_2.items[ItemType.BOOTS]
+        child_2.items[ItemType.BOOTS] = char_1.items[ItemType.BOOTS]
+        child_1.items[ItemType.BOOTS] = char_2.items[ItemType.BOOTS]
     elif i == 3:
-        new_character_2.items[ItemType.HELMET] = character_1.items[ItemType.HELMET]
-        new_character_1.items[ItemType.HELMET] = character_2.items[ItemType.HELMET]
+        child_2.items[ItemType.HELMET] = char_1.items[ItemType.HELMET]
+        child_1.items[ItemType.HELMET] = char_2.items[ItemType.HELMET]
     elif i == 4:
-        new_character_2.items[ItemType.GLOVES] = character_1.items[ItemType.GLOVES]
-        new_character_1.items[ItemType.GLOVES] = character_2.items[ItemType.GLOVES]
+        child_2.items[ItemType.GLOVES] = char_1.items[ItemType.GLOVES]
+        child_1.items[ItemType.GLOVES] = char_2.items[ItemType.GLOVES]
     elif i == 5:
-        new_character_2.items[ItemType.ARMOUR] = character_1.items[ItemType.ARMOUR]
-        new_character_1.items[ItemType.ARMOUR] = character_2.items[ItemType.ARMOUR]
+        child_2.items[ItemType.ARMOUR] = char_1.items[ItemType.ARMOUR]
+        child_1.items[ItemType.ARMOUR] = char_2.items[ItemType.ARMOUR]
 
 
-def _maintain_point(character_1, character_2, new_character_1, new_character_2, i):
+def _maintain_point(char_1, char_2, child_1, child_2, i):
     # Height, weapons, boots, helmets, gloves, armour
     if i == 0:
-        new_character_1.height = character_1.height
-        new_character_2.height = character_2.height
+        child_1.height = char_1.height
+        child_2.height = char_2.height
     elif i == 1:
-        new_character_1.items[ItemType.WEAPON] = character_1.items[ItemType.WEAPON]
-        new_character_2.items[ItemType.WEAPON] = character_2.items[ItemType.WEAPON]
+        child_1.items[ItemType.WEAPON] = char_1.items[ItemType.WEAPON]
+        child_2.items[ItemType.WEAPON] = char_2.items[ItemType.WEAPON]
     elif i == 2:
-        new_character_1.items[ItemType.BOOTS] = character_1.items[ItemType.BOOTS]
-        new_character_2.items[ItemType.BOOTS] = character_2.items[ItemType.BOOTS]
+        child_1.items[ItemType.BOOTS] = char_1.items[ItemType.BOOTS]
+        child_2.items[ItemType.BOOTS] = char_2.items[ItemType.BOOTS]
     elif i == 3:
-        new_character_1.items[ItemType.HELMET] = character_1.items[ItemType.HELMET]
-        new_character_2.items[ItemType.HELMET] = character_2.items[ItemType.HELMET]
+        child_1.items[ItemType.HELMET] = char_1.items[ItemType.HELMET]
+        child_2.items[ItemType.HELMET] = char_2.items[ItemType.HELMET]
     elif i == 4:
-        new_character_1.items[ItemType.GLOVES] = character_1.items[ItemType.GLOVES]
-        new_character_2.items[ItemType.GLOVES] = character_2.items[ItemType.GLOVES]
+        child_1.items[ItemType.GLOVES] = char_1.items[ItemType.GLOVES]
+        child_2.items[ItemType.GLOVES] = char_2.items[ItemType.GLOVES]
     elif i == 5:
-        new_character_1.items[ItemType.ARMOUR] = character_1.items[ItemType.ARMOUR]
-        new_character_2.items[ItemType.ARMOUR] = character_2.items[ItemType.ARMOUR]
+        child_1.items[ItemType.ARMOUR] = char_1.items[ItemType.ARMOUR]
+        child_2.items[ItemType.ARMOUR] = char_2.items[ItemType.ARMOUR]
 
 
-def one_point(character_1, character_2, items, point):
-    new_character_1, new_character_2 = _new_characters(character_1, character_2)
+def one_point(char_1, char_2, items, point):
+    child_1, child_2 = _new_characters(char_1, char_2)
 
     for i in range(5):
         if i < point:
-            _change_point(character_1, character_2, new_character_1, new_character_2, i)
+            _change_point(char_1, char_2, child_1, child_2, i)
         else:
-            _maintain_point(character_1, character_2, new_character_1, new_character_2, i)
+            _maintain_point(char_1, char_2, child_1, child_2, i)
 
-    return new_character_1, new_character_2
+    return child_1, child_2
 
 
-def two_points(character_1, character_2, items, point_1, point_2):
-    new_character_1, new_character_2 = _new_characters(character_1, character_2)
+def two_points(char_1, char_2, items, point_1, point_2):
+    child_1, child_2 = _new_characters(char_1, char_2)
 
     for i in range(5):
         if i < point_1 or i >= point_2:
-            _change_point(character_1, character_2, new_character_1, new_character_2, i)
+            _change_point(char_1, char_2, child_1, child_2, i)
         else:
-            _maintain_point(character_1, character_2, new_character_1, new_character_2, i)
+            _maintain_point(char_1, char_2, child_1, child_2, i)
 
-    return new_character_1, new_character_2
+    return child_1, child_2
 
 
-def uniform(character_1, character_2, items, probability=0.5):
-    new_character_1, new_character_2 = _new_characters(character_1, character_2)
+def uniform(char_1, char_2, items, probability=0.5):
+    child_1, child_2 = _new_characters(char_1, char_2)
 
     for i in range(5):
         if random() < probability:
-            _change_point(character_1, character_2, new_character_1, new_character_2, i)
+            _change_point(char_1, char_2, child_1, child_2, i)
 
-    return new_character_1, new_character_2
+    return child_1, child_2
 
 
-def anular(character_1, character_2, items):
-    new_character_1, new_character_2 = _new_characters(character_1, character_2)
+def anular(char_1, char_2, items):
+    child_1, child_2 = _new_characters(char_1, char_2)
     # TODO
-    return new_character_1, new_character_2
+    return child_1, child_2
 
 
 #TODO completar el diccionario
