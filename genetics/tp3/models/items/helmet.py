1635adcacd5106ec36a40419084da89ebd5bdffd
Juan Pablo Orsay <jporsay@gmail.com>
2017-05-27 19:13:15 -0300

    Delete unused classes

--- genetics/tp3/models/items/helmet.py
+++
@@ -1,7 +0,0 @@
-from .item import Item, ItemType
-
-
-class Helmet(Item):
-    def __init__(self, id: int, strength: float, agility: float, expertise: float, resistance: float, life: float):
-        super().__init__(id, strength, agility, expertise, resistance, life)
-        self._type = ItemType.HELMET
