361caf63aed7e5ce65acb09a73155015dcbbbe12
Juan Pablo Orsay <jporsay@gmail.com>
2017-06-06 11:58:18 -0300

    add jsonSchema

--- genetics/setup.py
+++ genetics/setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=[
         'numpy',
         'matplotlib',
-        'jsonschema',
+        'jsonschema'
     ],
 
     # List additional groups of dependencies here (e.g. development
