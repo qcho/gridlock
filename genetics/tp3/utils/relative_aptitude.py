a632ba5c793eb5769e587a739f863f2d34f1e245
Juan Pablo Orsay <jporsay@gmail.com>
2017-06-06 15:34:05 -0300

    directly mapping

--- genetics/tp3/utils/relative_aptitude.py
+++ genetics/tp3/utils/relative_aptitude.py
@@ -2,5 +2,5 @@ import numpy as np
 
 
 def relative_aptitude(population):
-    all_fitness = np.sum(list(map(lambda x: x.fitness, population)))
-    return list(map(lambda x: x.fitness / all_fitness, population))
+    all_fitness = np.sum([x.fitness for x in population])
+    return [x.fitness / all_fitness for x in population]
