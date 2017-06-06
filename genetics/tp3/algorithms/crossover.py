274d86551edfc49c3ead73ef4a2558ca34cb6097
Juan Pablo Orsay <jporsay@gmail.com>
2017-06-06 11:31:44 -0300

    Type-hinting returns

--- genetics/tp3/algorithms/crossover.py
+++ genetics/tp3/algorithms/crossover.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from ..models.items import ItemType
 from ..models.characters import Character
 from random import random
@@ -6,7 +8,7 @@ from random import randint
 GENES = 6
 
 
-def _spawn_children(char_1, char_2):
+def _spawn_children(char_1: Character, char_2: Character) -> Tuple[Character, Character]:
     return char_1.spawn(), char_2.spawn()
 
 
@@ -18,7 +20,7 @@ def _swap_height(child_1: Character, child_2: Character):
     child_1.height, child_2.height = child_2.height, child_1.height
 
 
-def one_point(char_1: Character, char_2: Character):
+def one_point(char_1: Character, char_2: Character) -> Tuple[Character, Character]:
     point = randint(1, GENES-1)
     child_1, child_2 = _spawn_children(char_1, char_2)
     if point == 0:
@@ -30,7 +32,7 @@ def one_point(char_1: Character, char_2: Character):
     return child_1, child_2
 
 
-def two_points(char_1: Character, char_2: Character):
+def two_points(char_1: Character, char_2: Character) -> Tuple[Character, Character]:
     point_1 = randint(0, GENES - 1)
     point_2 = randint(point_1, GENES - 1)
     child_1, child_2 = _spawn_children(char_1, char_2)
@@ -43,7 +45,7 @@ def two_points(char_1: Character, char_2: Character):
     return child_1, child_2
 
 
-def uniform(char_1: Character, char_2: Character):
+def uniform(char_1: Character, char_2: Character) -> Tuple[Character, Character]:
     probability = 0.5
     child_1, child_2 = _spawn_children(char_1, char_2)
     if random() < probability:
@@ -55,7 +57,7 @@ def uniform(char_1: Character, char_2: Character):
     return child_1, child_2
 
 
-def annular(char_1: Character, char_2: Character):
+def annular(char_1: Character, char_2: Character) -> Tuple[Character, Character]:
     child_1, child_2 = _spawn_children(char_1, char_2)
     point = randint(0, GENES)
     length = randint(0, GENES-1)
